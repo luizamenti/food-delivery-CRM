14334bf367d197c8606a1460d034627a2cae2e65
luizamenti <107424994+luizamenti@users.noreply.github.com>
2022-07-20 18:08:16 -0300

    Versão final
    Versão final

--- projeto.js
+++ projeto.js
@@ -1,18 +1,70 @@
+// Lista pratos e indexes
+listaPratoIndex = [[1, 'Risoto'], [2, 'Lasagna'], [3, 'Pizza']]
+
+//Pratos disponíveis
+const risoto = {
+    id: 1,
+    nomePrato: 'Risoto',
+    preco: 40,
+    pontosAcumulados: 20
+}
+
+const lasagna = {
+    id: 2,
+    nomePrato: 'Lasagna',
+    preco: 50,
+    pontosAcumulados: 25
+}
+
+const pizza = {
+    id: 3,
+    nomePrato: 'Pizza',
+    preco: 60,
+    pontosAcumulados: 30
+}
+
+let listaPratos = []
+listaPratos.push(risoto, lasagna, pizza)
+
+let campoValor = document.getElementById('mostrar_valor')
+let campoPontos = document.getElementById('mostrar_pontos')
+let botaoValorPontos = document.getElementById('botao_mostrar_valor')
+
+function mostrarValorEPontos () {
+    let indexPratoSelecionado = document.getElementById('pratos').selectedIndex
+    for (let i = 0; i < listaPratos.length; i++) {
+        if (listaPratos[i].id === indexPratoSelecionado) {
+            let valorDoPrato = listaPratos[i].preco
+            let pontosDoPrato = listaPratos[i].pontosAcumulados
+            campoValor.setAttribute('value', valorDoPrato)
+            campoPontos.setAttribute('value', pontosDoPrato)
+        }
+    }
+}
+
+botaoValorPontos.addEventListener('click', mostrarValorEPontos)
+
 function clicarBotao () {
     let nome = document.getElementById('nome').value
     let email = document.getElementById('email').value
     let celular = document.getElementById('celular').value
-    let cidade = document.getElementById('cidade').value
+    let bairro = document.getElementById('bairro').value
     let rua = document.getElementById('rua').value
     let numero = document.getElementById('numero').value
+    let pratoSelecionado = document.getElementById('pratos').selectedIndex
+    let valorPratoEscolhido = campoValor.getAttribute('value')
+    let pontosPratoEscolhido = campoPontos.getAttribute('value')
 
     localStorage.setItem('cadastro', JSON.stringify({
         'nome': nome,
         'email': email,
         'celular': celular,
-        'cidade': cidade,
+        'bairro': bairro,
         'rua': rua,
-        'numero': numero
+        'numero': numero,
+        'prato': pratoSelecionado,
+        'valor': valorPratoEscolhido,
+        'pontos': pontosPratoEscolhido
     }))
 }
 
--- projeto2.js
+++ projeto2.js
@@ -1,7 +1,7 @@
 // Resgatar JSON 'cadastro' e transformar em objeto 'infosSalvas'
 let infosCadastro = JSON.parse(localStorage.getItem('cadastro'))
 
-// Designar infos do cadastro aos campos desabilitados
+// Designar infos do objeto cadastro aos campos de cadastro
 let nome2 = document.getElementById('nome2')
 nome2.setAttribute('value', infosCadastro.nome)
 
@@ -11,11 +11,35 @@ email2.setAttribute('value', infosCadastro.email)
 let celular2 = document.getElementById('celular2')
 celular2.setAttribute('value', infosCadastro.celular)
 
-let cidade2 = document.getElementById('cidade2')
-cidade2.setAttribute('value', infosCadastro.cidade)
+let bairro2 = document.getElementById('bairro2')
+bairro2.setAttribute('value', infosCadastro.bairro)
 
 let rua2 = document.getElementById('rua2')
 rua2.setAttribute('value', infosCadastro.rua)
 
 let numero2 = document.getElementById('numero2')
 numero2.setAttribute('value', infosCadastro.numero)
+
+objIndexPrato = {
+    1: 'Risoto',
+    2: 'Lasagna',
+    3: 'Pizza'
+}
+
+let nomePrato = objIndexPrato[infosCadastro.prato]
+
+// Designar infos do objeto cadastro aos campos de histórico
+let tabela = document.getElementById('tabela')
+
+let newRow = tabela.insertRow(-1)
+let newCell = newRow.insertCell(-1)
+let newText = document.createTextNode(nomePrato)
+newCell.appendChild(newText)
+
+let newCell2 = newRow.insertCell(-1)
+let newText2 = document.createTextNode(infosCadastro.valor)
+newCell2.appendChild(newText2)
+
+let newCell3 = newRow.insertCell(-1)
+let newText3 = document.createTextNode(infosCadastro.pontos)
+newCell3.appendChild(newText3)
